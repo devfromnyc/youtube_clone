4c4dab257533f9cea4e8fc085c21fd53503709d1
devfromnyc <devfromnyc@gmail.com>
2023-08-31 05:10:17 -0400

    feat: created searchbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import { Feed } from "@mui/icons-material";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => {
-  <BrowserRouter>
+  return (
+    <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }} />
     <Navbar />
     <Routes>
@@ -18,7 +22,8 @@ const App = () => {
       <Route path="/channel/:id" element={<ChannelDetail />} />
       <Route path="/search/:searchTerm" element={<SearchFeed />} />
     </Routes>
-  </BrowserRouter>;
+  </BrowserRouter>
+  )
 };
 
 export default App;
--- src/components/VideDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VideDetail = () => {
-  return (
-    <div>VideDetail</div>
-  )
-}
-
-export default VideDetail
