e8aa79cc173f897e882e9855253b07d503b54877
devfromnyc <devfromnyc@gmail.com>
2023-09-03 03:28:19 -0400

    feat: created search feed with video and channel results

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromApi } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box
+      p={2}
+      sx={{ overflowY: "auto", height: "100vh", flex: 2, background: "#000" }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results For{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
